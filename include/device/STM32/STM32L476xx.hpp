c410da01f8d40463d78e67390b23bba53d8e11c5
Peter Züger <zueger.peter@icloud.com>
2018-08-13 00:12:26 +0200

    added RCC registers for STM32L476xx

--- include/device/STM32/STM32L476xx.hpp
+++ include/device/STM32/STM32L476xx.hpp
@@ -38,6 +38,49 @@ namespace let{
             constexpr auto LCKK         = 1 << 16;// Lock key
         }
     }
+
+    namespace RCC{
+        constexpr auto RCC              = 0x40021000;
+
+        namespace R{
+            constexpr auto CR           = 0x00;   //
+            constexpr auto ICSCR        = 0x04;   //
+            constexpr auto CFGR         = 0x08;   //
+            constexpr auto PLLCFGR      = 0x0C;   //
+            constexpr auto PLLSAI1CFGR  = 0x10;   //
+            constexpr auto PLLSAI2CFGR  = 0x14;   //
+            constexpr auto CIER         = 0x18;   //
+            constexpr auto CIFR         = 0x1C;   //
+            constexpr auto CICR         = 0x20;   //
+            constexpr auto AHB1RSTR     = 0x28;   //
+            constexpr auto AHB2RSTR     = 0x2C;   //
+            constexpr auto AHB3RSTR     = 0x30;   //
+            constexpr auto APB1RSTR1    = 0x38;   //
+            constexpr auto APB1RSTR2    = 0x3C;   //
+            constexpr auto APB2RSTR     = 0x40;   //
+            constexpr auto AHB1ENR      = 0x48;   //
+            constexpr auto AHB2ENR      = 0x4C;   //
+            constexpr auto AHB3ENR      = 0x50;   //
+            constexpr auto APB1ENR1     = 0x58;   //
+            constexpr auto APB1ENR2     = 0x5C;   //
+            constexpr auto APB2ENR      = 0x60;   //
+            constexpr auto AHB1SMENR    = 0x68;   //
+            constexpr auto AHB2SMENR    = 0x6C;   //
+            constexpr auto AHB3SMENR    = 0x70;   //
+            constexpr auto APB1SMENR1   = 0x78;   //
+            constexpr auto APB1SMENR2   = 0x7C;   //
+            constexpr auto APB2SMENR    = 0x80;   //
+            constexpr auto CCIPR        = 0x88;   //
+            constexpr auto BDCR         = 0x90;   //
+            constexpr auto CSR          = 0x94;   //
+            // constexpr auto CRRCR        = 0x98;
+            // constexpr auto CCIPR2       = 0x9C;
+        }
+
+        namespace B{
+
+        }
+    }
 }
 
 #endif /* __STM32L476XX_HPP__ */
