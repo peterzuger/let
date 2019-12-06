35ddbce87f66d51a3305657a232a8d0314a00694
Peter Züger <zueger.peter@icloud.com>
2019-12-06 09:59:55 +0100

    added RCC header, includes all Peripheral specific RCC headers

---
+++ include/RCC.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file   let/include/RCC.hpp
+ * @author Peter Züger
+ * @date   14.08.2018
+ * @brief  Reset and Clock Control abstraction
+ *
+ * This file is part of let (https://gitlab.com/peterzuger/let).
+ * Copyright (c) 2019 Peter Züger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LET_RCC_HPP
+#define LET_RCC_HPP
+
+#include "RCC/CRC.hpp"
+#include "RCC/DMA.hpp"
+#include "RCC/GPIO.hpp"
+#include "RCC/RNG.hpp"
+#include "RCC/SPI.hpp"
+#include "RCC/USART.hpp"
+
+#endif /* LET_RCC_HPP */
